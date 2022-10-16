2cdf5e162e47b3c268cffde308b558e34960e0e7
jhannka <jjhann.meza@gmail.com>
2022-10-16 15:17:27 -0500

    CRUD de Drivers y  se elimino la carpeta service

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,80 +3,45 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use App\Service\RespuestaJson;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Validator\Constraints\Email;
-use Symfony\Component\Validator\Validation;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 #[Route('/api')]
 class UserController extends AbstractController
 {
 
     #[Route('/user-register', name: 'user-register', methods: 'POST')]
-    public function register(Request $request, RespuestaJson $resjson, ManagerRegistry $doctrine)
+    public function register(Request $request, ManagerRegistry $doctrine, UserPasswordHasherInterface $passwordHasher)
     {
-        $json = $request->get('json', null);
-        $params = json_decode($json);
+        $user = new User();
+        $data = json_decode($request->getContent(), true);
+        $form = $this->createForm(UserType::class, $user);
+        $form->submit($data);
 
-        $data = [
-            'status' => 'error',
-            'code' => 200,
-            'message' => 'El usuario no se ha creado',
-            'json' => $json
-        ];
 
-        if ($json != null) {
-            $username = (!empty($params->username)) ? $params->username : null;
-            $email = (!empty($params->email)) ? $params->email : null;
-            $password = (!empty($params->password)) ? $params->password : null;
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $form->get('password')->getData()
+        );
 
-            $validator = Validation::createValidator();
-            $validateEmail = $validator->validate($email, [
-                new  Email()
-            ]);
+        $user->setPassword($hashedPassword);
 
-            if (!empty($username) && !empty($email) && count($validateEmail) == 0 && !empty($password)) {
-                $user = new User();
-                $user->setUsername($username);
-                $user->setEmail($email);
-                $user->setRoles(['ROLE_USER']);
+        $user->setRoles(['ROLE_USER']);
+        $em = $doctrine->getManager();
+        $em->persist($user);
+        $em->flush();
 
-                $pwd = hash('sha256', $password);
-                $user->setPassword($pwd);
-
-                $em = $doctrine->getManager();
-
-                $userRepo = $doctrine->getRepository(User::class);
-                $issetUser = $userRepo->findBy(array(
-                    'email' => $email
-                ));
-
-                if (count($issetUser) == 0) {
-
-                    $em->persist($user);
-                    $em->flush();
-
-                    $data = [
-                        'status' => 'succes',
-                        'code' => 200,
-                        'message' => 'El usuario se ha creado',
-                        'json' => $user
-
-                    ];
-                } else {
-                    $data = [
-                        'status' => 'error',
-                        'code' => 400,
-                        'message' => 'El usuario ya existe',
-                        'json' => $json
-                    ];
-                }
-            }
-        }
-        return $resjson->resjson($data);
+        return new JsonResponse(
+            [
+                'status' => 'ok'
+            ], JsonResponse::HTTP_CREATED
+        );
     }
 }
